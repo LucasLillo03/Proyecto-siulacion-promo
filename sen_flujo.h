1dab53d57482ddb56d23b08774af2ad17e575da4
unknown <lucaslillo0207@gmail.com>
2026-06-16 16:41:59 -0300

    creacion del sensor de flujo y conexion con el codigo

---
+++ sen_flujo.h
@@ -0,0 +1,25 @@
+//CPP:mySource/Proyecto-siulacion-promo/sen_flujo.cpp
+#if !defined sen_flujo_h
+#define sen_flujo_h
+
+#include "simulator.h"
+#include "event.h"
+#include "stdarg.h"
+
+
+
+class sen_flujo: public Simulator { 
+// Declare the state,
+// output variables
+// and parameters
+
+public:
+	sen_flujo(const char *n): Simulator(n) {};
+	void init(double, ...);
+	double ta(double t);
+	void dint(double);
+	void dext(Event , double );
+	Event lambda(double);
+	void exit();
+};
+#endif
