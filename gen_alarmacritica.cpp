acd3ddb2def77b40097eb3f6ba35aeefa1022d97
saguasantiago <saguasantiago@gmail.com>
2026-06-18 14:48:34 -0300

    codidgo de alarma critica

---
+++ gen_alarmacritica.cpp
@@ -0,0 +1,41 @@
+#include "gen_alarmacritica.h"
+void gen_alarmacritica::init(double t,...) {
+//The 'parameters' variable contains the parameters transferred from the editor.
+va_list parameters;
+va_start(parameters,t);
+//To get a parameter: %Name% = va_arg(parameters,%Type%)
+//where:
+//      %Name% is the parameter name
+//	%Type% is the parameter type
+sigma = 0;
+alarm = ALARMA_CRITICA;
+}
+double gen_alarmacritica::ta(double t) {
+//This function returns a double.
+return sigma;
+}
+void gen_alarmacritica::dint(double t) {
+sigma = 1000;
+}
+void gen_alarmacritica::dext(Event x, double t) {
+//The input event is in the 'x' variable.
+//where:
+//     'x.value' is the value (pointer to void)
+//     'x.port' is the port number
+//     'e' is the time elapsed since last transition
+
+}
+Event gen_alarmacritica::lambda(double t) {
+//This function returns an Event:
+//     Event(%&Value%, %NroPort%)
+//where:
+//     %&Value% points to the variable which contains the value.
+//     %NroPort% is the port number (from 0 to n-1)
+
+
+return Event(&alarm, 0);
+}
+void gen_alarmacritica::exit() {
+//Code executed at the end of the simulation.
+
+}
---
+++ gen_alarmacritica.h
@@ -0,0 +1,29 @@
+//CPP:source/gen_alarmacritica.cpp
+#if !defined gen_alarmacritica_h
+#define gen_alarmacritica_h
+
+#include "simulator.h"
+#include "event.h"
+#include "stdarg.h"
+
+#include "../mysources/tipos.h"
+#include "../mysources/constantes.h"
+
+
+class gen_alarmacritica: public Simulator { 
+// Declare the state,
+// output variables
+// and parameters
+
+double sigma;
+AlarmaCaudal alarm;
+public:
+	gen_alarmacritica(const char *n): Simulator(n) {};
+	void init(double, ...);
+	double ta(double t);
+	void dint(double);
+	void dext(Event , double );
+	Event lambda(double);
+	void exit();
+};
+#endif
