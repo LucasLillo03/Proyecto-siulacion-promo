6c9caa60c6e865fdf1979481ac214241300bad67
saguasantiago <saguasantiago@gmail.com>
2026-06-20 13:12:31 -0300

    valores constantes para los tipos de alarmas y estados de bolsa para comunicacion entre DEVS

--- constantes.h
+++ constantes.h
@@ -2,4 +2,13 @@
 #define constants_h
 static const double INF_VAL = 1.0e+30;
 
+#define VALOR_ALARMA_BAJA 0.0
+#define VALOR_ALARMA_MEDIA 1.0
+#define VALOR_ALARMA_CRITICA 2.0
+#define VALOR_ALARMA_APAGADA 3.0
+
+
+#define VALOR_BOLSA_NORMAL 0
+#define VALOR_BOLSA_AGOTANDOSE 1.0
+#define VALOR_BOLSA_VACIA 2.0
 #endif
